79e15a784c0be6747dc216b4e9c1618bad67ac4d
David Ingram <david@ingramaviation.com>
2019-09-11 17:33:50 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,16 +25,6 @@ class Scraper
       
       social_icons = page.css(".social-icon-container a").collect{|icon| icon.atttribute("href").value}
       social_icons.each do |link|
-      
-      # <div class="social-icon-container">
-      #     <a href="https://twitter.com/empireofryan"><img class="social-icon" src="../assets/img/twitter-icon.png"></a>
-      #     <a href="https://www.linkedin.com/in/ryan-johnson-321629ab"><img class="social-icon" src="../assets/img/linkedin-icon.png"></a>
-      #     <a href="https://github.com/empireofryan"><img class="social-icon" src="../assets/img/github-icon.png"></a>
-      #     <a href="https://www.youtube.com/watch?v=C22ufOqDyaE"><img class="social-icon" src="../assets/img/rss-icon.png"></a>
-      #   </div>
-
-      container = page.css(".social-icon-container a").collect{|icon| icon.attribute("href").value}
-      container.each do |link|
         if link.include?("twitter")
           student[:twitter] = link
         elsif link.include?("linkedin")
@@ -44,6 +34,10 @@ class Scraper
         elsif link.include?(".com")
           student[:blog] = link
         end
+        
+      
+      
+      
       end
       student[:profile_quote] = page.css(".profile-quote").text
       student[:bio] = page.css("div.description-holder p").text
